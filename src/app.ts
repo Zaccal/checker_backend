83b7adc9255a87f5e48c7f94627763dee94b4888
Adlet <ksss90411@gmail.com>
2025-06-26 00:28:04 +0500

    feat: changed status error for protected route middleware

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Routes
 app.route("/todos", todosApp);
-app.route("/todos/subtasks", subTaskApp);
+app.route("/subtasks", subTaskApp);
 app.route("/lists", tasksList);
 app.route("/tags", tagsApp);
 app.route("/auth", authApp);
