340991479a098532cfa0df6622eb852c90cc230e
Adlet <ksss90411@gmail.com>
2025-07-06 14:35:00 +0500

    feat: init edge api

---
+++ api/index.ts
@@ -0,0 +1,15 @@
+import { handle } from "@hono/node-server/vercel";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import { app } from "../dist/src/app.js";
+
+export const runtime = "edge";
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PATCH = handle(app);
+export const PUT = handle(app);
+export const DELETE = handle(app);
+export const HEAD = handle(app);
+export const OPTIONS = handle(app);
