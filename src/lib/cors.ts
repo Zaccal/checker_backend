519afe6bcaad9df175fc0f4d3010ee5abc9e2440
Adlet <ksss90411@gmail.com>
2025-07-10 22:56:12 +0500

    fix: fixed cors 🪛

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -1,7 +1,7 @@
 import type { CORSOptions } from "hono/cors";
 
 export const authCors: CORSOptions = {
-  origin: process.env.ORIGINS!.split(" "),
+  origin: process.env.ORIGINS!.split(",") || [],
   allowHeaders: ["Content-Type", "Authorization"],
   allowMethods: ["POST", "GET", "OPTIONS"],
   exposeHeaders: ["Content-Length"],
@@ -10,7 +10,7 @@ export const authCors: CORSOptions = {
 };
 
 export const globalCors: CORSOptions = {
-  origin: process.env.ORIGINS!.split(" ") || [],
+  origin: process.env.ORIGINS!.split(",") || [],
   allowHeaders: ["Content-Type", "Authorization", "Accept"],
   allowMethods: ["POST", "GET", "PUT", "DELETE", "OPTIONS", "PATCH"],
   exposeHeaders: ["Content-Length", "X-Request-ID"],
